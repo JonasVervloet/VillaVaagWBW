3813b1143272c26f81ae4eff7d42045e32e73ff0
JonasVervloet <vervloetjonas@hotmail.com>
2020-07-29 23:19:45 +0200

    added  semicolons

--- app.js
+++ app.js
@@ -2,15 +2,14 @@ const express = require('express');
 
 const app = express();
 
-
 //Route
 app.get('/', (req, res) => {
     res.send('We are on home!!');
-})
+});
 
 app.get('/posts', (req, res) => {
     res.send('We are on posts!');
-})
+});
 
 // Listen to porst 3000.
 app.listen(3000);
