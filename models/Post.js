0b60d35b5160e42d6eaca76467702e3fb41111df
JonasVervloet <vervloetjonas@hotmail.com>
2020-07-30 21:12:33 +0200

    Save post to db

--- models/Post.js
+++ models/Post.js
@@ -14,6 +14,6 @@ const PostSchema = mongoose.Schema({
         type: Date,
         default: Date.now
     }
-})
+});
 
-module.exports = mongoose.model('Posts', PostSchema)
+module.exports = mongoose.model('Posts', PostSchema);
--- routes/posts.js
+++ routes/posts.js
@@ -1,6 +1,5 @@
-const express = require('express');
-const router = express.Router();
-const Post = require('../models/Post')
+const router = require('express').Router();
+const Post = require('../models/Post');
 
 
 router.get('/', (req, res) => {
@@ -8,8 +7,19 @@ router.get('/', (req, res) => {
 });
 
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
     console.log(req.body);
+    var post = new Post({
+        title: req.body.title,
+        description: req.body.description
+    });
+
+    try {
+        const savedPost = await post.save();
+        res.json(savedPost);
+    } catch (err) {
+        res.status(400).send(err);
+    }
 })
 
 module.exports = router;
