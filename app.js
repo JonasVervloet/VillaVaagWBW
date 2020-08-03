300e71f8f4a2c4afdba065e850e30b45036f7369
JonasVervloet <vervloetjonas@hotmail.com>
2020-08-03 14:17:53 +0200

    add cors

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 require('dotenv/config');
 
 const app = express();
+app.use(cors());
 app.use(bodyParser.json());
 
 //Import routes
