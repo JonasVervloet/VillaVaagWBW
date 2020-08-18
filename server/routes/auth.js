264f62164f965cb333cd2572b3092cf4df851d51
JonasVervloet <vervloetjonas@hotmail.com>
2020-08-18 21:04:46 +0200

    add email exist validation

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -3,11 +3,21 @@ const User = require('../models/User');
 const {registerValidation, loginValidation} = require('../validations/auth_validation')
 
 router.post('/register', async (req, res) => {
+    // Validate request body
     const {error} = registerValidation.validate(req.body);
     if (error) {
         return res.status(400).send(error.details[0].message);
     }
 
+    // Check if email doesn't already exists
+    const emailExists = await User.findOne({
+        email: req.body.email
+    });
+    if (emailExists) {
+        return res.status(400).send('Email already exists!');
+    }
+
+    // Create user
     const user = new User({
         name: req.body.name,
         lastName: req.body.lastName,
@@ -15,6 +25,7 @@ router.post('/register', async (req, res) => {
         password: req.body.password
     });
 
+    // Save user to database
     try {
         const savedUser = await user.save();
         res.send(savedUser);
