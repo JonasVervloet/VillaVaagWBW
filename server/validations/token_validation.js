d51ed5e084eddd3859d0b2233670699b31cb0c84
JonasVervloet <vervloetjonas@hotmail.com>
2020-08-19 14:52:03 +0200

    debug authentication

--- server/validations/token_validation.js
+++ server/validations/token_validation.js
@@ -5,14 +5,14 @@ module.exports = function(req, res, next) {
     const token = req.header('auth-token');
 
     if (! token) {
-        res.status(401).send('Access Denied! No token present...');
+        return res.status(401).send('Access Denied! No token present...');
     }
 
     try {
         const tokenBody = jwt.verify(token, process.env.TOKEN_SECRET);
         req.user = tokenBody;
     } catch (error) {
-        res.status(401).send('Access Denied! Invalid token...');
+        return res.status(401).send('Access Denied! Invalid token...');
     }
 
     next();
